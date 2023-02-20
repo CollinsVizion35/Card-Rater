c8e5d83de88dd408e8f0a30ad42d2f67d942bfc4
Collins Vizion <okechukwuchibuikecollins@gmail.com>
2023-02-20 03:41:41 +0100

    began the floor edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, createContext } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import "./App.css";
 import Home from "./home";
 
--- src/home.jsx
+++ src/home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import Floor from "./Floor";
+
+function ManShow() {
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      <color args={[0, 0, 0.001]} attach="background" />
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Floor/>
+    </>
+  );
+}
 
 function Home() {
   return (
-    <div></div>
-  )
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <ManShow />
+      </Canvas>
+    </Suspense>
+  );
 }
 
-export default Home
+export default Home;
